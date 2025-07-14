5aed2a21de7dfb40e28d33e68953c64d987c4c42
deepanshuTTN <deepanshu.mishra@tothenew.com>
2025-07-14 22:31:12 +0530

    add util to use as dependency in other project

---
+++ src/main/java/com/spring/redisdemo/utils/SomeUtil.java
@@ -0,0 +1,8 @@
+package com.spring.redisdemo.utils;
+
+public class SomeUtil {
+    public static void getMagicText() {
+        System.out.println("Hello from the other side :D");
+        System.out.println("~deepansu");
+    }
+}
